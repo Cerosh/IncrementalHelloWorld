37580b04f79a7a5c4f9ee74e28c196ebfd69c324
Cerosh <ceroshjacob@gmail.com>
2021-03-06 13:17:13 +1100

    Initial check-in of module IncrementalHelloWorld

---
+++ IncrementalHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IncrementalHelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
